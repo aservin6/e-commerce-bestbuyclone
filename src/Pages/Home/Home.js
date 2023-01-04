eee4064a50caa099fadfb13df9a5ac73e12c60d9
aservin6 <servin.alejandroii@gmail.com>
2023-01-04 11:12:34 -0600

    fixed mobile responsiveness

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,10 +5,26 @@ import OutletDealsWidget from "../../components/Widgets/OutletDealsWidget";
 import TodaysTopPicksWidget from "../../components/Widgets/TodaysTopPicksWidget";
 import TrendingProducts from "../../components/Widgets/TrendingProducts";
 import WidgetContainer from "../../components/Widgets/WidgetContainer";
+import { useState } from "react";
 
 const Home = () => {
+  const [showDisclaimer, setShowDisclaimer] = useState(true);
+
+  const hideDisclaimer = () => setShowDisclaimer(false);
   return (
     <>
+      {showDisclaimer && (
+        <div
+          onClick={hideDisclaimer}
+          className="fixed top-0 left-0 z-50 w-full h-screen bg-opacity-80 bg-dark"
+        >
+          <span className="fixed z-50 w-full px-3 text-lg font-semibold text-center text-white -translate-x-1/2 -translate-y-1/2 md:max-w-4xl md:text-2xl top-1/3 left-1/2">
+            Disclaimer: This projects API account is currently undergoing an
+            approval process, some products may not load due to restricted API
+            access. Click screen to continue.
+          </span>
+        </div>
+      )}
       <Wrapper bgColor={"bg-white"}>
         <section id="widget-section" className="px-3 bg-lightGray lg:px-0">
           <WidgetContainer>
